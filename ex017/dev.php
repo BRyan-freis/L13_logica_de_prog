5c227057906fa75e293cca1f58753dfd687c5ba9
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-02-19 10:09:52 -0300

    Update dev.php

--- ex017/dev.php
+++ ex017/dev.php
@@ -23,6 +23,39 @@
     $produto1 = new Produto("Camiseta", 39.90);
     // Constructor chamado aqui
     $produto2 = new Produto("Tênis", 300.00);
+    $produto1->exibirDetalhes();
+    $produto2->exibirDetalhes();
+
+    //Quando o script termina ou o objeto sai de escopo, o destruor é chamado
+    unset($produto1); //Chama o destrutor explicitamente
+
+
+
+    //mais uma classe construct e destruct
+
+    class ConexaoBanco{
+        private $conexao;
+
+    //Construtor: simula abrir a conexão com o banco
+        public function __construct(){
+            $this->conexao = "Conexão ao banco de dados aberta!";
+            echo $this->conexao . "<br>";
+        }
+
+    //Destrutor: simula fechar a conexão com o banco
+        public function __destruct(){
+            // Tempo de espera (em segundos)
+            sleep(2); // Espera 2 segundos antes de exibir a mensagem
+
+         echo "Conexão ao banco de dados fechada! <br>";   
+        }
+    }
+
+    //Criando um objeto de conexão
+    $conectado = new ConexaoBanco(); //Construtor é chamado aqui
+
+    unset($conexao); //chama o destrutor explicitamente
+
     ?>
 </body>
 </html>
